de05732dbb95793dbabf3bbd95589bcfe85892b7
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-30 22:47:27 +0100

    refactor and handle edge cases in 'Tic_tac_toe' controller

--- application/controllers/Tic_tac_toe.php
+++ application/controllers/Tic_tac_toe.php
@@ -2,10 +2,14 @@
 
 class Tic_tac_toe extends CI_Controller
 {
+    var $errors = array();
+    var $validation_passed;
 
     public function __construct()
     {
         parent::__construct();
+        // TODO: load libraries/helpers/models only when they are needed? (performance?)
+        $this->load->library('form_validation');
         $this->load->library('session');
         $this->load->helper('form');
         $this->load->helper('url');
@@ -16,30 +20,28 @@ class Tic_tac_toe extends CI_Controller
     public function begin()
     {
 
-        $validation_result = $this->_perform_validation();
 
         // if form is filled correctly then go to PLAY
         // this is the most common scenario
-        if ($validation_result === TRUE)
+        $this->validation_passed = $this->_perform_validation();
+        if ($this->validation_passed)
         {
-            $this->_go_to_play();
-            return;
+            $this->_redirect_to_play();
         }
+
+        $this->_check_flash_data();
+
+        $this->_check_session();
 
         // if form was sent but doesn't validate
         // then make sure an error message will be shown
-        $should_show_error = $this->_form_was_sent() AND $validation_result === FALSE;
+        $this->_check_form();
 
+        $data['title'] = 'Players\' selection';
 
         // prepare all sections of page
-        $data['title'] = 'Players\' selection';
         $this->load->view('templates/header', $data);
-
-        if ($should_show_error)
-        {
-            $this->_show_error();
-        }
-
+        $this->_show_errors_if_any($data);
         $this->load->view('ttt/begin', $data);
         $this->load->view('templates/footer', $data);
     }
@@ -51,19 +53,58 @@ class Tic_tac_toe extends CI_Controller
         return $this->form_validation->run();
     }
 
+    private function _check_flash_data()
+    {
+        $last_error = $this->session->flashdata('last_error');
+        if ($last_error !== NULL)
+        {
+            $this->_add_error($last_error);
+        }
+    }
+
+    private function _check_session()
+    {
+        $challenge_id = $this->session->userdata('ch_id');
+        if ($challenge_id !== NULL)
+        {
+            $play_link = anchor('tic-tac-toe/play', 'playing');
+            $msg = 'If you continue you will create a new session and current '
+                .'results will be lost.<br />'
+                .'Go to ' . $play_link . ' instead.';
+            $this->_add_error($msg);
+        }
+    }
+
+    private function _check_form()
+    {
+        $form_invalid = $this->_form_was_sent() AND $this->validation_passed === FALSE;
+        if ($form_invalid)
+        {
+            $msg = 'Both players must have names!';
+            $this->_add_error($msg);
+        }
+    }
+
     private function _form_was_sent()
     {
         return $this->input->post('submit') !== NULL;
     }
 
-    private function _show_error()
+    private function _add_error($msg)
     {
-        $msg = 'Both players must have names!';
-        $data['error_message'] = $msg;
-        $this->load->view('templates/error', $data);
+        $this->errors[] = $msg;
     }
 
-    private function _go_to_play()
+    private function _show_errors_if_any(&$data)
+    {
+        if ( ! empty($this->errors))
+        {
+            $data['error_messages'] = $this->errors;
+            $this->load->view('templates/error', $data);
+        }
+    }
+
+    private function _redirect_to_play()
     {
         $player1 = $this->input->post('player1');
         $player2 = $this->input->post('player2');
@@ -71,11 +112,20 @@ class Tic_tac_toe extends CI_Controller
         $challenge_id = $this->challenge_model->create_challenge($player1, $player2);
         $this->session->set_userdata('ch_id', $challenge_id);
 
+        $this->session->keep_flashdata('last_error');
         redirect('tic-tac-toe/play');
     }
 
     public function play()
     {
+
+        if ($this->session->userdata('ch_id') === NULL)
+        {
+            $msg = 'First, name your players!';
+            $this->session->set_flashdata('last_error', $msg);
+            redirect('tic-tac-toe/begin');
+        }
+
 
         $data['title'] = "Play";
         $this->load->view('templates/header', $data);
--- application/views/templates/error.php
+++ application/views/templates/error.php
@@ -1,4 +1,6 @@
 
-<div style="color:red; margin:50px">
-    <?php echo $error_message ?>
-</div>
+<?php foreach ($error_messages as $error_item): ?>
+    <div style="color:red; margin:50px">
+        <?php echo $error_item ?>
+    </div>
+<?php endforeach; ?>
