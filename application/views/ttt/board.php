dc64bd3dfd865b287dabf273548889dacf297082
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-07-01 23:36:03 +0100

    add mapping string to board's state

--- application/views/ttt/board.php
+++ application/views/ttt/board.php
@@ -1,53 +1,80 @@
 
+<?php
+    if ( ! isset($board_state)) {
+        // TODO: change to '_________'
+        $board_state = 'ooo_oxoxo';
+    }
+
+    $mapping = str_split($board_state);
+    for ($i=0 ; $i<9 ; $i++)
+    {
+        switch ($mapping[$i])
+        {
+            case 'x':
+                $mapping[$i] = 'x';
+                break;
+            case 'o':
+                $mapping[$i] = 'o';
+                break;
+            case '_':
+                $mapping[$i] = 'empty';
+                break;
+        }
+        $format_string = '<img src="/assets/img/%s.svg" />';
+        $mapping[$i] = sprintf($format_string, $mapping[$i]);
+    }
+    $pos = 0;
+?>
+
 <table class="board">
     <tr>
         <td>
             <div class="content left-edge top-edge">
-                <img src="/assets/img/o.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
         <td>
             <div class="content top-edge">
-                <img src="/assets/img/o.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
         <td>
             <div class="content right-edge top-edge">
-                <img src="/assets/img/o.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
     </tr>
     <tr>
         <td>
             <div class="content left-edge">
-                <img src="/assets/img/empty.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
         <td>
             <div class="content">
-                <img src="/assets/img/o.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
         <td>
             <div class="content right-edge">
-                <img src="/assets/img/x.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
     </tr>
     <tr>
         <td>
             <div class="content left-edge bottom-edge">
-                <img src="/assets/img/o.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
         <td>
             <div class="content bottom-edge">
-                <img src="/assets/img/x.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
         <td>
             <div class="content right-edge bottom-edge">
-                <img src="/assets/img/o.svg"/>
+                <?php echo $mapping[$pos++]; ?>
             </div>
         </td>
     </tr>
