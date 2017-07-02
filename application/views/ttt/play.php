06732a63a49d484a2f4688fddeed9ffd52033273
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-07-02 15:24:54 +0100

    improve UI in 'play'

--- application/views/ttt/play.php
+++ application/views/ttt/play.php
@@ -1,15 +1,12 @@
 
 
-<div class="container-fluid"
->
+<div class="container" >
 
     <div class="row">
         <!-- Play board -->
         <div class="play-section
                     col-xs-12
-                    col-sm-offset-0 col-sm-8
-                    col-md-offset-1 col-md-7
-                    col-lg-offset-1 col-lg-6">
+                    col-sm-offset-0 col-sm-8">
             <h2>Play</h2>
             <?php
                 $this->load->view('ttt/board', ['clickable' => TRUE]);
@@ -18,10 +15,8 @@
 
         <!-- Recent boards -->
         <div class="recent-section
-                    col-xs-offset-1 col-xs-10
-                    col-sm-offset-1 col-sm-3
-                    col-md-offset-2 col-md-2
-                    col-lg-offset-3 col-lg-2">
+                    col-xs-offset-2 col-xs-8
+                    col-sm-offset-1 col-sm-3">
             <h3>Recent games</h3>
             <?php
                 foreach ($recent_games as $game)
