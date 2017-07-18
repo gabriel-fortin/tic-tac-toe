3268ef1870ba1a79b529d9d64c564bb4c102ac80
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-07-18 13:16:43 +0100

    collect all SQL snippets into one file

---
+++ model.sql
@@ -0,0 +1,23 @@
+
+CREATE DATABASE main_db;
+
+USE main_db;
+
+CREATE TABLE challenge (
+    id int NOT NULL AUTO_INCREMENT,
+    string_id varchar(32) UNIQUE,
+    player1 varchar(128) NOT NULL,
+    player2 varchar(128) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE game (
+    challenge_id int,
+    board_state varchar(9) NOT NULL,
+    timestamp timestamp NOT NULL,
+    FOREIGN KEY(challenge_id)
+        REFERENCES challenge(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
